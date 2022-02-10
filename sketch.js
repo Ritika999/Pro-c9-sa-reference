7c4dcaa9566f924879d72e080fbbb32095a1fb4e
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2022-02-10 16:33:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,28 +10,7 @@ function draw()
 {
    background(30);
 
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    box.position.x = box.position.x +5;
-  }
-
-    if (keyIsDown(LEFT_ARROW)) 
-  {
-    box.position.x= box.position.x -5;
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    box.position.y = box.position.y - 5;
-   
-  }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    box.position.y = box.position.y + 5;
-  }
-
+  
   drawSprites();
 }
 
